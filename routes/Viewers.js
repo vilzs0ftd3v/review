5aabcca8d5708a212bed02ed295dd47b8d1a3fc3
nebuchadnezar <vilzs0ftd3v@gmail.com>
2024-12-02 12:31:51 +0800

    update on server.js

--- routes/Viewers.js
+++ routes/Viewers.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/fetch',(require,response) => {
+router.get('/fetch',(request,response) => {
     //response.send("List of viewers")    
-    response.json({
+    /*response.json({
         "name":"nebuchadnezar",
         "age":"50"
 
-    })    
+    })*/
+   //response.send(request.query);
+   console.log(request.body);
 })
 
 router.post('/insert',(request,response) => {
